b9de8ff1580bbb0f52ae1a99a3f5980e8c2e7e34
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-08 19:57:27 +0530

    Added:linked list by adding 30 and 56 to 70

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+public class LinkedListMain {
+    public static void main(String[] args){
+        LinkedList<Integer> list1=new LinkedList();
+        list1.push(70);
+        list1.push(30);
+        list1.push(56);
+        list1.show();
+    }
+}
