e7546b36cc76a4ce2ceffdac23041a4e7954aec5
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-08 17:04:05 +0530

    Added:create a simple linked list with data 56,30 and 70

---
+++ src/com/bridgelabz/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linkedlist;
+/**
+ *creating single node
+ */
+public class Node<T> {
+    T data;
+    Integer s;
+    Node<T> next;
+    public Node(){
+    }
+    public void compare(){
+    }
+    public Node(T data){
+        this.data=data;
+    }
+}
