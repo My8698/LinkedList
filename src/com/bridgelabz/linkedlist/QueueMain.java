59f465db4bea0e54ef2d3f504a48e6209e837fcb
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-11 11:57:36 +0530

    Added:ability to dequeue Queue elements using LinkedList

--- src/com/bridgelabz/linkedlist/QueueMain.java
+++ src/com/bridgelabz/linkedlist/QueueMain.java
@@ -14,7 +14,14 @@ public class QueueMain {
         queueObject.enQueue(56);
         queueObject.enQueue(30);
         queueObject.enQueue(70);
-        System.out.println("***** Display Queue Elements *****");
+        System.out.println("Display Queue Elements");
         queueObject.displayQueue();
-    }
+        System.out.println(" Display Queue Elements\n ");
+        queueObject.displayQueue();
+        System.out.println(" Display deQueue Elements\n ");
+        queueObject.deQueue(56);
+        queueObject.displayQueue();
+        System.out.println(" Display deQueue Elements\n ");
+        queueObject.deQueue(30);
+        queueObject.displayQueue();}
 }
